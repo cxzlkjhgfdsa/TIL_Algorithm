081903226b97310a6c24241c554949306e4c2524
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2024-01-19 01:50:32 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.12 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        String[] str = my_string.split(" ");
+        
+        answer += Integer.parseInt(str[0]);
+        
+        for(int i = 2; i < str.length; i+=2){
+            if(str[i-1].equals("+")){
+                answer += Integer.parseInt(str[i]);
+            }else{
+                answer -= Integer.parseInt(str[i]);
+            }
+        }
+        
+        return answer;
+    }
+}
