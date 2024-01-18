732d4f5efebc1aa0e49f755c05eba2ce0780e05a
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2024-01-19 04:28:17 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.07 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+        
+        
+        
+        while(chicken >= 10){
+            
+            chicken -= 9;
+            answer++;
+        }
+
+        
+        return answer;
+    }
+}
