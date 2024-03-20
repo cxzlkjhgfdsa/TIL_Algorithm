bf81d7991c4d19c26ac8894f007802dbe8dfdcd0
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2024-03-20 21:57:39 +0900

    [Silver II] Title: 한 줄로 서기, Time: 120 ms, Memory: 12932 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,79 +1,24 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    static int[] nums;
-    static int N;
-    static int[] role;
-    static boolean[] visited;
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-        nums = new int[N];
-        role = new int[N];
-        visited = new boolean[N+1];
-
-        for(int i = 0; i < N; i++){
-            role[i] = sc.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int [] tall = new int[n+1];
+        List<Integer> ans = new ArrayList<>();
+ 
+        for(int i=1; i<=n; i++) {
+            tall[i] = scan.nextInt();
         }
-
-        perm(0);
-
-    }
-
-    private static void perm(int cnt){
-
-        if(cnt == N){
-            if(check()){
-                for(int i = 0; i < N; i++){
-                    System.out.print(nums[i]+" ");
-                }
-                System.exit(0);
-            }
-            return;
+ 
+        for(int i=n; i>=1; i--) {
+            ans.add(tall[i], i);
         }
-
-
-        for(int i = 1; i <=N; i++){
-
-            if(visited[i])
-                continue;
-
-            nums[cnt] = i;
-            visited[i] = true;
-            perm(cnt+1);
-            visited[i] = false;
-
+ 
+        for(int k : ans) {
+            System.out.print(k+" ");
         }
-    }
-
-    private static boolean check(){
-
-        boolean ch = true;
-        for(int i = 0; i < N; i++){
-            int cur = nums[i];
-
-            if(i == 0){
-                if(role[cur-1] != 0){
-                    ch = false;
-                    break;
-                }
-            }
-            int cnt = 0;
-            for(int j = i-1; j >=0; j--){
-                if(nums[j] > cur)
-                    cnt++;
-            }
-            if(cnt != role[cur-1]){
-                ch = false;
-                break;
-            }
-        }
-
-        return ch;
     }
 
 }
