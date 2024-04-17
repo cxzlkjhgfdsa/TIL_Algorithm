022792a2dc54fb7b377ce82dd646f14f0c37ade1
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2024-04-17 12:41:46 +0900

    [Silver V] Title: 큰 수 구성하기, Time: 320 ms, Memory: 24612 KB -BaekjoonHub

---
+++ 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static int N, K, size, max;
+    static int[] arr;
+    static int[] nums;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        size = String.valueOf(N).length();
+
+        arr = new int[K];
+        nums = new int[size];
+        for (int i = 0; i < K; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        max = 0;
+
+        cal(0);
+
+        System.out.println(max);
+    }
+    public static void cal(int cnt){
+        if(cnt > 0){
+            String s = "";
+            for(int i = 0; i < cnt; i++){
+                s += nums[i];
+            }
+            //System.out.println(s);
+            if(Integer.parseInt(s) <= N) {
+                max = Math.max(max, Integer.parseInt(s));
+            }
+        }
+        if(cnt == size)
+            return;
+
+
+        for(int i = 0; i < K; i++){
+            nums[cnt] = arr[i];
+            cal(cnt+1);
+        }
+    }
+}
