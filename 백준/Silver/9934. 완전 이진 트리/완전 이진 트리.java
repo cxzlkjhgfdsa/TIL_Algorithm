ada70eee094336bea22f9d1a80687a5c7e055b71
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2024-05-16 13:34:37 +0900

    [Silver I] Title: 완전 이진 트리, Time: 168 ms, Memory: 16380 KB -BaekjoonHub

---
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    static int k;
+    static int[] arr;
+    static List<Integer>[] tree;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        k = sc.nextInt();
+
+        arr = new int[(int)Math.pow(2, k) - 1];
+        tree =new ArrayList[k];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < k; i++){
+            tree[i] = new ArrayList<>();
+        }
+
+        findNode(0, arr.length-1, 0);
+
+        for(int i = 0; i < k; i++){
+            for(Integer n : tree[i]){
+                sb.append(n).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    private static void findNode(int start, int end, int floor) {
+
+        if(floor == k)
+            return;
+
+        int root = (start + end) / 2;
+        tree[floor].add(arr[root]);
+
+        findNode(start, root -1, floor + 1);
+        findNode(root + 1, end, floor + 1);
+    }
+
+
+}
