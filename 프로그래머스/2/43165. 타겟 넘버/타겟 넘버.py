f15273e2eb90ad8687b114f6dc3a9d09954dcfb5
cxzlkjhgfdsa <96561194+cxzlkjhgfdsa@users.noreply.github.com>
2025-05-12 08:52:34 +0900

    [level 2] Title: 타겟 넘버, Time: 367.58 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,14 @@
-answer = 0
-
 def solution(numbers, target):
-    global answer
-    dfs(0, 0,numbers, target)
+    answer = dfs(0, 0,numbers, target)
     return answer
 
 
 def dfs(cur_num, index, numbers, target):
-    global answer
     if index == len(numbers):
         if cur_num == target:
-            answer += 1
+            return 1
         return 0
     
-    dfs(cur_num+numbers[index], index+1, numbers, target)
-    dfs(cur_num-numbers[index], index+1, numbers, target)
+    return dfs(cur_num+numbers[index], index+1, numbers, target) + dfs(cur_num-numbers[index], index+1, numbers, target)
+    
     
